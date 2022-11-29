6899a1df804473fc8be20936fb55408fd257a7c5
jose.silva <ronierison.silva@gmail.com>
2022-11-29 12:45:43 -0300

    refactor: improve code and add search functionalities

--- search/binary.go
+++ search/binary.go
@@ -4,7 +4,57 @@ import (
 	"math"
 )
 
-func findFirstItemBinarySearch(arr []int, e int) int {
+/*
+ * This methods receives an array of integers and a element to search on this array.
+ * It will return the index of the the element in the array (not necessarely the lower index).
+ * If it does not find the element, it will return -1.
+ * Obs.: It requires a ordered array.
+ * Parameters:
+ * 	- arr []int (ex.: []int{1,2,3,5,5,6,7,8,9})
+ * 	- e int (ex.: 5)
+ * 	-> output: 3
+ */
+func FindItemBinarySearch(arr []int, element int) int {
+	return abstractBinarySearch(arr, element, anyElement)
+}
+
+/**
+ * This methods receives an array of integers and a element to search on this array.
+ * It will return the index of the first occurrence of the element in the ordered array.
+ * If it does not find the element, it will return -1.
+ * Obs.: It requires a ordered array.
+ * Parameters:
+ * 	- arr []int (ex.: []int{1,2,3,5,5,6,7,8,9})
+ * 	- e int (ex.: 5)
+ * 	-> output: 4
+ */
+func FindFirstItemBinarySearch(arr []int, element int) int {
+	return abstractBinarySearch(arr, element, firstElement)
+}
+
+/**
+ * This methods receives an array of integers and a element to search on this array.
+ * It will return the index of the last occurrence of the element in the ordered array.
+ * If it does not find the element, it will return -1.
+ * Obs.: It requires a ordered array.
+ * Parameters:
+ * 	- arr []int (ex.: []int{1,2,3,5,5,6,5,8,9})
+ * 	- e int (ex.: 5)
+ * 	-> output: 6
+ */
+func FindLastItemBinarySearch(arr []int, element int) int {
+	return abstractBinarySearch(arr, element, lastElement)
+}
+
+type searchType int
+
+const (
+	firstElement searchType = iota
+	anyElement
+	lastElement
+)
+
+func abstractBinarySearch(arr []int, element int, searchType searchType) int {
 	left := 0
 	right := len(arr) - 1
 	lastFound := -1
@@ -16,13 +66,22 @@ func findFirstItemBinarySearch(arr []int, e int) int {
 	for left <= right {
 		curr := int(math.Floor((float64(left) + float64(right)) / 2))
 
-		if arr[curr] < e {
+		if arr[curr] < element {
 			left = curr + 1
-		} else if arr[curr] > e {
+		} else if arr[curr] > element {
 			right = curr - 1
 		} else {
-			right = curr - 1
-			lastFound = curr
+
+			switch searchType {
+			case firstElement:
+				right = curr - 1
+				lastFound = curr
+			case anyElement:
+				return curr
+			case lastElement:
+				left = curr + 1
+				lastFound = curr
+			}
 		}
 
 		if curr == lastIndexLeft || curr == lastIndexRight {
--- search/binary_test.go
+++ search/binary_test.go
@@ -6,50 +6,157 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSearchOfAnInexistentElement(t *testing.T) {
+// pure binary search tests
+func TestSearchOfAnInexistentElementSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{1, 2, 2, 3, 3, 3, 4, 6}
 
-	index := findFirstItemBinarySearch(arr, 9)
+	index := FindItemBinarySearch(arr, 9)
 
 	assert.Equal(t, -1, index)
 }
 
-func TestSearchOfAnUniqElement(t *testing.T) {
+func TestSearchOfAnUniqElementSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 6}
 
-	index := findFirstItemBinarySearch(arr, 3)
+	index := FindItemBinarySearch(arr, 3)
 
 	assert.Equal(t, 2, index)
 }
 
-func TestSearchOfDuplicatedElement(t *testing.T) {
+func TestSearchOfDuplicatedElementSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{1, 2, 3, 3, 3, 4, 6}
 
-	index := findFirstItemBinarySearch(arr, 3)
+	index := FindItemBinarySearch(arr, 3)
 
-	assert.Equal(t, 2, index)
+	assert.Equal(t, 3, index)
 }
 
-func TestSearchOfManyDuplicatedElement(t *testing.T) {
+func TestSearchOfManyDuplicatedElementSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{3, 3, 3, 3, 3, 3, 3}
 
-	index := findFirstItemBinarySearch(arr, 3)
+	index := FindItemBinarySearch(arr, 3)
 
-	assert.Equal(t, 0, index)
+	assert.Equal(t, 3, index)
 }
 
-func TestSearchOfManyDuplicatedOnRightElement(t *testing.T) {
+func TestSearchOfManyDuplicatedOnRightElementSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{1, 2, 2, 2, 2, 3, 3}
 
-	index := findFirstItemBinarySearch(arr, 3)
+	index := FindItemBinarySearch(arr, 3)
 
 	assert.Equal(t, 5, index)
 }
 
-func TestSearchOfElementRightPosition(t *testing.T) {
+func TestSearchOfElementRightPositionSearchingForAnyOccurrence(t *testing.T) {
 	arr := []int{1, 2, 2, 2, 2, 1, 3}
 
-	index := findFirstItemBinarySearch(arr, 3)
+	index := FindItemBinarySearch(arr, 3)
 
 	assert.Equal(t, 6, index)
+}
+
+// binary search for first occurrence (worst complexity case) tests
+func TestSearchOfAnInexistentElementSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 3, 3, 3, 4, 6}
+
+	index := FindFirstItemBinarySearch(arr, 9)
+
+	assert.Equal(t, -1, index)
+}
+
+func TestSearchOfAnUniqElementSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 6}
+
+	index := FindFirstItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 2, index)
+}
+
+func TestSearchOfDuplicatedElementSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{1, 2, 3, 3, 3, 4, 6}
+
+	index := FindFirstItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 2, index)
+}
+
+func TestSearchOfManyDuplicatedElementSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{3, 3, 3, 3, 3, 3, 3}
+
+	index := FindFirstItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 0, index)
+}
+
+func TestSearchOfManyDuplicatedOnRightElementSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 2, 2, 3, 3}
+
+	index := FindFirstItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 5, index)
+}
+
+func TestSearchOfElementRightPositionSearchingForFirstOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 2, 2, 1, 3}
+
+	index := FindFirstItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 6, index)
+}
+
+// binary search for last occurrence (worst complexity case) tests
+func TestSearchOfAnInexistentElementSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 3, 3, 3, 4, 6}
+
+	index := FindLastItemBinarySearch(arr, 9)
+
+	assert.Equal(t, -1, index)
+}
+
+func TestSearchOfAnUniqElementSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 6}
+
+	index := FindLastItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 2, index)
+}
+
+func TestSearchOfDuplicatedElementSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 3, 3, 3, 4, 6}
+
+	index := FindLastItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 4, index)
+}
+
+func TestSearchOfManyDuplicatedElementSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{3, 3, 3, 3, 3, 3, 3}
+
+	index := FindLastItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 6, index)
+}
+
+func TestSearchOfManyDuplicatedOnRightElementSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 2, 2, 3, 3}
+
+	index := FindLastItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 6, index)
+}
+
+func TestSearchOfElementRightPositionSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 2, 2, 1, 3}
+
+	index := FindLastItemBinarySearch(arr, 3)
+
+	assert.Equal(t, 6, index)
+}
+
+func TestSearchOfElementLeftPositionSearchingForLastOccurrence(t *testing.T) {
+	arr := []int{1, 2, 2, 2, 3, 3, 4}
+
+	index := FindLastItemBinarySearch(arr, 1)
+
+	assert.Equal(t, 0, index)
 }
